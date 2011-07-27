917ab59f4a56081d9bcced07d13620bafd46ae30
joselpm21@gmail.com <joselpm21@gmail.com@ea32cc08-60d7-ed69-808b-ffe600edc202>
2011-07-27 15:45:53 +0000

    Sala de Chat.

--- trunk/CodeSolidsProject/src/codesolids/gui/chat/ChatGui.java
+++ trunk/CodeSolidsProject/src/codesolids/gui/chat/ChatGui.java
@@ -1,23 +1,34 @@
 package codesolids.gui.chat;
 
+import java.sql.ResultSet;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import nextapp.echo.app.Alignment;
 import nextapp.echo.app.ApplicationInstance;
+import nextapp.echo.app.Border;
 import nextapp.echo.app.Button;
 import nextapp.echo.app.Color;
 import nextapp.echo.app.Column;
 import nextapp.echo.app.Component;
+import nextapp.echo.app.ContentPane;
 import nextapp.echo.app.Extent;
 import nextapp.echo.app.FillImage;
 import nextapp.echo.app.FillImageBorder;
+import nextapp.echo.app.Grid;
 import nextapp.echo.app.ImageReference;
+import nextapp.echo.app.Insets;
 import nextapp.echo.app.Label;
+import nextapp.echo.app.Pane;
+import nextapp.echo.app.Panel;
 import nextapp.echo.app.ResourceImageReference;
 import nextapp.echo.app.Row;
 import nextapp.echo.app.SplitPane;
+import nextapp.echo.app.Style;
 import nextapp.echo.app.TaskQueueHandle;
 import nextapp.echo.app.TextArea;
 import nextapp.echo.app.Window;
@@ -27,17 +38,24 @@ import nextapp.echo.app.event.ActionListener;
 import nextapp.echo.app.event.WindowPaneEvent;
 import nextapp.echo.app.event.WindowPaneListener;
 
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 import codesolids.bd.clases.Usuario;
 import codesolids.bd.hibernate.SessionHibernate;
 import codesolids.bd.clases.Chat;
 import codesolids.gui.academia.TimedServerPush;
 
+import codesolids.gui.mapa.MapaDesktop;
 import codesolids.gui.principal.PrincipalApp;
 import codesolids.gui.style.GUIStyles;
+import echopoint.HtmlLayout;
+import echopoint.layout.HtmlLayoutData;
 
 /**
  * 
@@ -45,78 +63,168 @@ import codesolids.gui.style.GUIStyles;
  * 
  */
 
-public class ChatGui extends WindowPane {
+public class ChatGui extends ContentPane {
 
 	private TextArea txtCharla;
 	private TextArea txtMsg;
-	private SplitPane splitPane2;
+	private TextArea txtUsuarios;
 	private Column col;
+	private Column col2;
+	private Usuario usuario;
 
 	TaskQueueHandle taskQueue;
 	TimedServerPush timedServerPush;
-
-	// private Session session;
+	private HtmlLayout htmllayaut;
 
 	private Chat chatBean;
 
 	public ChatGui() {
 		taskQueue = ApplicationInstance.getActive().createTaskQueue();
-		initGUI();
+		add(initGUI());
 	}
 
 	// --------------------------------------------------------------------------------
 
-	public void initGUI() {
+	public Component initGUI() {
+		
+		
+		txtCharla = new TextArea();
+		txtCharla.setEditable(false);
+		txtCharla.setInsets(new Insets(15, 15, 15, 15));
+		txtCharla.setWidth(new Extent(540));
+		txtCharla.setHeight(new Extent(335));
+
+		txtUsuarios = new TextArea();
+		txtUsuarios.setEditable(false);
+		txtUsuarios.setHeight(new Extent(360));
+		txtUsuarios.setWidth(new Extent(155));
+
+		col2 = new Column();
+
+		this.setBackground(Color.BLACK);
+
+		try {
+			htmllayaut = new HtmlLayout(getClass().getResourceAsStream("index.html"), "UTF-8");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+
+		HtmlLayoutData hld;
+		hld = new HtmlLayoutData("chat");
+
 
 		ApplicationInstance app = ApplicationInstance.getActive();
-		
+
 		chatBean = new Chat();
 
-		ResourceImageReference w = new ResourceImageReference("Images/Fondos/usuario.jpg");	
-		ImageReference image = w;
-		FillImage imagef = new FillImage(image);
-		
-		
-		this.setTitle("Chat");
-		this.setMinimumHeight(new Extent(300));
-		this.setMinimumWidth(new Extent(650));
-		this.setResizable(false);
-		this.setBackgroundImage(imagef);
-		
+
 		txtMsg = new TextArea();
-		txtMsg.setWidth(new Extent(400));
+		txtMsg.setMaximumLength(220);
+		txtMsg.setWidth(new Extent(512));
+		txtMsg.setHeight(new Extent(145));
 
-		SplitPane splitPane = new SplitPane();
-		splitPane.setSeparatorWidth(new Extent(5));
-		splitPane.setSeparatorColor(Color.BLUE);
-		splitPane.setSeparatorPosition(new Extent(150));
-		//splitPane.set
-		splitPane.setResizable(true);
 
-		splitPane.add(mostrarActivos());
-
-		splitPane2 = new SplitPane();
-		splitPane2.setSeparatorWidth(new Extent(5));
-		splitPane2.setOrientation(splitPane2.ORIENTATION_VERTICAL);
-		splitPane2.setSeparatorPosition(new Extent(210));
-		splitPane2.setResizable(true);
-		splitPane2.setSeparatorColor(Color.BLUE);
-				
-		col = new Column();
-		
-		splitPane2.add(col);
-		
 		Row row = new Row();
+		row.setCellSpacing(new Extent(20));
 
 		txtMsg.setText(txtMsg.getText());
 
 		row.add(txtMsg);
 
+		row.add(txtMsg);
+
+		col = new Column();
+
+		mostrarMensajes();
+		mostrarActivos();
+		
+		timedServerPush = new TimedServerPush(1000, app, taskQueue,
+				new Runnable() {
+			@Override
+			public void run() {
+				mostrarMensajes();
+				mostrarActivos();
+
+			}
+		});
+
+		timedServerPush.beg();
+
+		//		addWindowPaneListener(new WindowPaneListener() {
+		//
+		//			@Override
+		//			public void windowPaneClosing(WindowPaneEvent arg0) {
+		//				timedServerPush.end();
+		//
+		//			}
+		//		});
+
+		Column col3 = new Column();
+
+		col3.add(initPanel());
+
+
+		//col.add(mostrarActivos());
+
+		col3.setLayoutData(hld);
+		htmllayaut.add(col3);
+
+		hld = new HtmlLayoutData("banner2");
+
+		Button btnRegresar = new Button("REGRESAR");
+		btnRegresar.setStyle(GUIStyles.DEFAULT_STYLE);
+		btnRegresar.setHeight(new Extent(20));
+		btnRegresar.setAlignment(Alignment.ALIGN_BOTTOM);
+		btnRegresar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				btnRegresarCliked();
+
+			}
+		});
+
+		btnRegresar.setLayoutData(hld);
+		htmllayaut.add(btnRegresar);
+
+		return htmllayaut;
+
+	}
+
+
+	public Column initPanel(){
+
+		Panel panel = new Panel();
+
+		ResourceImageReference w = new ResourceImageReference("Images/Fondos/marco1.png");	
+		ImageReference image = w;
+		FillImage imagef = new FillImage(image);
+
+		panel.setWidth(new Extent(801));
+		panel.setHeight(new Extent(535));
+
+		panel.setBackgroundImage(imagef);
+
+		Column colp = new Column();
+		colp.setCellSpacing(new Extent(90));
+
+		Column colp2 = new Column();
+		Row row = new Row();
+
+		row.setCellSpacing(new Extent(30));
+		row.add(panelMensajes());
+		row.add(panelUsuarios());
+		colp2.add(row);
+
+		colp.add(colp2);
+
+		row = new Row();
+		row.setCellSpacing(new Extent(50));
+		row.add(panelTexto());
 		Button btnEnviar = new Button("Enviar");
 		btnEnviar.setStyle(GUIStyles.DEFAULT_STYLE);
-
-		row.add(txtMsg);
-		row.add(btnEnviar);
+		btnEnviar.setHeight(new Extent(25));
 
 		btnEnviar.addActionListener(new ActionListener() {
 
@@ -127,49 +235,95 @@ public class ChatGui extends WindowPane {
 			}
 		});
 
-		splitPane2.add(row);
-		
-		
-		splitPane.add(splitPane2);
+		row.add(btnEnviar);
 
-		// Configuration configuration = new Configuration();
-		// configuration.configure("hibernate.cfg.xml");
-		// SessionFactory sessionFactory = configuration.buildSessionFactory();
+		colp.add(row);
 
-		// --------------------------------------------------------------------------------
+		panel.add(colp);
 
-		// session = sessionFactory.openSession();
-		// session.beginTransaction();
+		colp = new Column();
+		colp.add(panel);
 
-		timedServerPush = new TimedServerPush(1000, app, taskQueue,
-				new Runnable() {
-					@Override
-					public void run() {
-						mostrarMensajes();
+		return colp;
 
-					}
-				});
-
-		timedServerPush.beg();
-		
-		addWindowPaneListener(new WindowPaneListener() {
-			
-			@Override
-			public void windowPaneClosing(WindowPaneEvent arg0) {
-				timedServerPush.end();
-				
-			}
-		});
-		
-		
-		add(splitPane);
 
 	}
 
-	
-public Component mostrarActivos() {
+	public Component panelMensajes(){
 
-		Column col = new Column();
+		Panel panel = new Panel();
+
+		panel.setHeight(new Extent(360));
+		panel.setWidth(new Extent(585));
+		//panel.setBackground(Color.GREEN);
+		panel.setAlignment(Alignment.ALIGN_LEFT);
+
+		Column colp = new Column();
+		colp.setInsets(new Insets(5, 5, 5, 5));
+		colp.add(txtCharla);
+
+		panel.add(colp);
+
+		colp = new Column();
+		//colp.setBorder(new Border(5, Color.RED, 1));
+		colp.add(panel);
+
+		return colp;
+
+
+	}
+
+	public Component panelUsuarios(){
+
+		Panel panel = new Panel();
+
+		panel.setHeight(new Extent(360));
+		panel.setWidth(new Extent(786 - 585));
+		//panel.setBackground(Color.GREEN);
+
+		panel.setAlignment(Alignment.ALIGN_LEFT);
+
+		Column colp = new Column();
+		colp.setInsets(new Insets(15, 10, 10, 10));
+		colp.add(txtUsuarios);
+
+		panel.add(colp);
+
+		colp = new Column();
+		colp.add(panel);
+
+		return colp;
+
+
+	}
+
+	public Component panelTexto(){
+
+		Panel panel = new Panel();
+
+		panel.setHeight(new Extent(512-450));
+		panel.setWidth(new Extent(532));
+		//panel.setBackground(Color.GREEN);
+
+		panel.setAlignment(Alignment.ALIGN_LEFT);
+
+		Column colp = new Column();
+		colp.setInsets(new Insets(15, 0, 5, 5));
+		colp.add(txtMsg);
+
+		panel.add(colp);
+
+		colp = new Column();
+		colp.add(panel);
+
+		return colp;
+
+
+	}
+
+	public void mostrarActivos() {
+
+		txtUsuarios.setText("");
 
 		Session session = SessionHibernate.getInstance().getSession();
 		session.beginTransaction();
@@ -179,42 +333,49 @@ public Component mostrarActivos() {
 
 		List<Usuario> list = session.createCriteria(Usuario.class).list();
 		for (Usuario obj : list) {
-			
-			
-			System.out.println(obj.getLogin());
-			col.add(new Label(obj.getLogin()));
+
+
+			if ( obj.isActivo() == true ){
+			txtUsuarios.setText( obj.getLogin() + "\n" + txtUsuarios.getText());
+
+			}
+
 		}
+		
 		session.getTransaction().commit();
 		session.close();
 
-		return col;
 	}
 
-	@SuppressWarnings("deprecation")
+
 	public void mostrarMensajes() {
 
-		col.removeAll();
-		
+		txtCharla.setText("");
+
 		Session session = SessionHibernate.getInstance().getSession();
 		session.beginTransaction();
 
-		List<codesolids.bd.clases.Chat> list = session.createCriteria(codesolids.bd.clases.Chat.class).list();
-		
+		Criteria criteria;
+
+
+		//criteria = session.createCriteria(codesolids.bd.clases.Chat.class).addOrder(Order.desc("dateMsg")).list();
+		List<codesolids.bd.clases.Chat> list =  session.createCriteria(codesolids.bd.clases.Chat.class).addOrder(Order.asc("id")).list();
+
+
+
 		for (codesolids.bd.clases.Chat obj : list) {
-			
-			
+
 			if ( obj.getDateMsg().get(Calendar.DAY_OF_MONTH) == Calendar.getInstance().get(Calendar.DAY_OF_MONTH) ){
 
-			
-			System.out.println(obj.getMensaje());
-			col.add(new Label( obj.getLogin() + ">>" + obj.getMensaje() ));
+				txtCharla.setText( obj.getLogin() + ">>" + obj.getMensaje() + "\n" + txtCharla.getText());
+
 			}
 		}
 
 		session.getTransaction().commit();
 		session.close();
 	}
-	
+
 	private void btnEnviarClicked() {
 
 		Session session = SessionHibernate.getInstance().getSession();
@@ -224,22 +385,51 @@ public Component mostrarActivos() {
 
 		Chat chatBean = new Chat();
 
-		
+
 		PrincipalApp pa = (PrincipalApp) ApplicationInstance.getActive();
 		Usuario usuario = pa.getUsuario();
 		usuario = (Usuario) session.load(Usuario.class, usuario.getId());
-		
+
 		chatBean.setMensaje(txtMsg.getText());
 		Calendar calendar = new GregorianCalendar();
 		chatBean.setLogin(usuario.getLogin());
 		chatBean.setDateMsg(calendar);
-		
+
+
 		session.save(chatBean);
 
 		session.getTransaction().commit();
 		session.close();
 
 		txtMsg.setText("");
+
+	}
+
+	protected void btnRegresarCliked() {
+		
+		Session session = null;
+		
+		session = SessionHibernate.getInstance().getSession();
+        session.beginTransaction();
+        
+
+		PrincipalApp pa = (PrincipalApp) ApplicationInstance.getActive();
+		Usuario usuario = pa.getUsuario();
+        
+        
+        Criteria criteria = session.createCriteria(Usuario.class).add(//
+                                               Restrictions.eq("login", usuario.getLogin()));
+
+        usuario = (Usuario) criteria.uniqueResult();
+		
+		usuario.setActivo(false);
+
+		session.getTransaction().commit();
+		session.close();
+
+		timedServerPush.end();
+		removeAll();
+		add(new MapaDesktop());
 
 	}
 
